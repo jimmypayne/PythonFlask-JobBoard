63a331b303f397ef6639530c0feb00fc8e042c1a
Jim Payne <jim@jims.zoomtown.com>
2020-03-25 12:25:30 -0400

    testing3

--- jobs/app.py
+++ jobs/app.py
@@ -1,12 +1,10 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    render_template('index.html')
+    return render_template('index.html')
 
-if __name__ == '__main__':
-   app.run()
+
