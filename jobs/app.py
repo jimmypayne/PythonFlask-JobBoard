e4f4de2f3191568bb69ce08298e9638a28a4ed97
Jim Payne <forrestpayne@outlook.com>
2020-03-27 09:59:03 -0400

    testing

--- jobs/app.py
+++ jobs/app.py
@@ -56,9 +56,9 @@ def review(employer_id):
         title = request.form['title']
         status = request.form['status']
 
-        date = datetime.now().strftime("%m/%d/%Y")
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
 
-#        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) values (?,?,?,?,?,?'), (review, rating, title, date, status, employer_id), commit=True)
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) values (?,?,?,?,?,?')', (review, rating, title, date, status, employer_id), commit=True)
 
         return redirect(url_for('employer', employer_id=employer_id))
 
